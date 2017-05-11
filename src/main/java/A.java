1cdaf68ecbaab1328ce691ef7c4df02359ae8076
Pawel <funky_pawel@o2.pl>
2017-05-11 20:52:09 +0200

    xx

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -8,6 +8,6 @@ public class A {
     public static void main(String...args){
 
         System.out.println("new methods added");
-        System.out.println("methid A");
+        System.out.println("Method E");
     }
 }
